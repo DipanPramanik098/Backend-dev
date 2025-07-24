a1993b19417d62e8e832c96bc7e2b626b1c1e5a3
DipanPramanik098 <myselfdipan6@gmail.com>
2025-07-24 16:58:19 +0530

    Backend

---
+++ _01_script.js
@@ -0,0 +1,29 @@
+// // let n = 5;
+// // console.log("n");
+
+// // let args = process.argv;
+// // for (let i = 2; i < args.length; i++) {
+// //   console.log("Hello ", args[i]);
+// // }
+
+// // console.log(process.argv);
+
+
+// const val = require('./math.js')
+// console.log(val)
+// console.log(val.sum(5,3))
+
+// // const fruits = require('./Fruits');
+
+// // console.log(fruits)
+// // console.log(fruits[0])
+// // console.log(fruits[0].name)
+
+
+
+// import
+
+import {sum,mul} from "./math.js"
+
+console.log(sum(6,9));
+console.log(mul(6,9));
---
+++ math.js
@@ -0,0 +1,20 @@
+// const sum = (a, b) => a + b;
+// const sub = (a, b) => a - b;
+// const mul = (a, b) => a * b;
+
+// const pi = 3.14;
+// let  obj = {
+//   sum : sum,
+//   mul:mul,
+//   pi:pi,
+//   sub:sub
+// };
+// require
+// module.exports = obj;
+
+
+// import
+
+export const sum = (a, b) => a + b;
+export const sub = (a, b) => a - b;
+export const mul = (a, b) => a * b;
